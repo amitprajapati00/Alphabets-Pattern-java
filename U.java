7e0c510f8dedebf9cca9fa430d77781ec69a062f
Amit kuma <124872168+amitprajapati00@users.noreply.github.com>
2024-09-19 00:06:02 +0530

    Add files via upload

---
+++ U.java
@@ -0,0 +1,23 @@
+class U
+{
+	public static void main(String... ak)
+	{
+		for(int i=1;i<=7;i++)
+		{
+			for(int j=1;j<=7;j++)
+			{
+				if(j==1 && i<7-1
+					|| (j == 7 - 1 && i < 7 - 1) 
+					||(i==7-1&& j > 0 && j < 7 - 2 ))
+					{
+						System.out.print(" *");
+					}
+					else
+					{
+						System.	out.print(" ");
+					}
+			}
+			System.out.println();
+		}
+	}
+}
